2224df077201472f6f134250968a0ab497113a5b
chichi <imchi1717@gmail.com>
2025-12-01 22:09:27 +0800

    新增菜單管理 - 菜單圖片新增

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 
 export const routes: Routes = [
   { path: 'tabs', component: TabsComponent },
-  { path: 'reservation', component: ReservationComponent }
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'menuAdmin', component: MenuAdminComponent}
+
 ];
---
+++ src/app/menu-admin/menu-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-menu-admin',
+  imports: [MatTabsModule],
+  templateUrl: './menu-admin.component.html',
+  styleUrl: './menu-admin.component.scss'
+})
+export class MenuAdminComponent {
+
+}
