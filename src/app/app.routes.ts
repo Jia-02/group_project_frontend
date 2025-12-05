7278527d4a155d91e8e4a1421c306d54eb443f67
DESKTOP-9UA1KLR\MaoJia <q9753a97534@gmail.com>
2025-12-05 14:39:39 +0800

    修正merge衝突

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,16 @@
 import { Routes } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
-<<<<<<< HEAD
 import { TableComponent } from './table/table.component';
 import { MealStatusComponent } from './meal-status/meal-status.component';
 import { MealStatusUserComponent } from './meal-status-user/meal-status-user.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 
 export const routes: Routes = [
   {path:'tabs',component:TabsComponent},
   {path:'table',component:TableComponent},
   {path:'meal/status',component:MealStatusComponent},
   {path:'meal/status/user',component:MealStatusUserComponent},
-=======
-import { ReservationComponent } from './reservation/reservation.component';
-import { MenuAdminComponent } from './menu-admin/menu-admin.component';
-
-export const routes: Routes = [
-  { path: 'tabs', component: TabsComponent },
   { path: 'reservation', component: ReservationComponent },
   { path: 'menuAdmin', component: MenuAdminComponent}
-
->>>>>>> reservation
-];
+]
