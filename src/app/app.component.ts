930dad3b376069e47390bbc890da384fce34c962
Jia <q9753a97534@gmail.com>
2025-12-12 14:30:33 +0800

    移除header在使用者餐點狀態頁面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './@component/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HeaderComponent],
+    HeaderComponent,
+    CommonModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'GroupProject';
 
-  ngOnInit(): void {
-
+  showHeader = true;
+  constructor(private router:Router){
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/meal/status/user') {
+          this.showHeader = false;
+        } else {
+          // console.log("NU")
+          this.showHeader = true;
+        }
+      }
+    });
   }
+
+
 
 }
 
