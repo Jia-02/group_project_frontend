d30cb35dc28106bdcc50d75fb14c549d2a786569
Jia <q9753a97534@gmail.com>
2025-12-12 12:27:25 +0800

    修改

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { TabsComponent } from './tabs/tabs.component';
 import { TableComponent } from './table/table.component';
 import { MealStatusComponent } from './meal-status/meal-status.component';
 import { MealStatusUserComponent } from './meal-status-user/meal-status-user.component';
-import { ReservationComponent } from './reservation/reservation.component';
+
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 import { CalendarComponent } from './calendar/calendar.component';
 
@@ -13,7 +13,6 @@ export const routes: Routes = [
   {path:'table',component:TableComponent},
   {path:'meal/status',component:MealStatusComponent},
   {path:'meal/status/user',component:MealStatusUserComponent},
-  { path: 'reservation', component: ReservationComponent },
   { path: 'menuAdmin', component: MenuAdminComponent},
   { path: 'calendar', component: CalendarComponent}
 
