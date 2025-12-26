3c0da0257d2b161751565a7c2b2386d0a2dcda5d
kelly <kellylove19971225@gmail.com>
2025-12-26 17:13:45 +0800

    123

--- src/app/@service/http-client.service.ts
+++ src/app/@service/http-client.service.ts
@@ -7,8 +7,8 @@ import { Injectable } from '@angular/core';
 export class HttpClientService {
 
   constructor(private http: HttpClient) { }
-  baseUrl = 'http://localhost:8080/';
-  // baseUrl="http://192.168.0.174:8080/";
+  // baseUrl = 'http://localhost:8080/';
+  baseUrl="http://192.168.0.174:8080/";
 
   // 讀取
   getApi(url: string) {
